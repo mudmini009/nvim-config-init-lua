c949f2c90116783d6bd2def60fafa2733f4e2762
Pollapaat  Suttimala <54277976+mudmini009@users.noreply.github.com>
2025-11-12 01:50:07 +0700

    C# for unity

--- init.lua
+++ init.lua
@@ -1,18 +1,20 @@
--- Disable annoying temp files
-vim.o.swapfile     = false
-vim.o.backup       = false
-vim.o.writebackup  = false
-vim.o.undofile     = false
+-- ===================================================================
+--  Your Basic Settings (Unchanged)
+-- ===================================================================
+vim.o.swapfile      = false
+vim.o.backup        = false
+vim.o.writebackup   = false
+vim.o.undofile      = false
 
 -- Basic UI Settings
-vim.opt.number         = true
-vim.opt.relativenumber = true
-vim.opt.tabstop        = 4
-vim.opt.shiftwidth     = 4
-vim.opt.expandtab      = true
-vim.opt.smartindent    = true
-vim.opt.termguicolors  = true
-vim.o.mouse            = "a"
+vim.opt.number          = true
+vim.opt.relativenumber  = true
+vim.opt.tabstop         = 4
+vim.opt.shiftwidth      = 4
+vim.opt.expandtab       = true
+vim.opt.smartindent     = true
+vim.opt.termguicolors   = true
+vim.o.mouse             = "a"
 
 -- Set leader key
 vim.g.mapleader = " "
@@ -21,18 +23,11 @@ vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
 
 -- Gemini AI: Auto-reload files when they change on disk
--- This section makes Neovim automatically reload files if they are
--- changed by an external program. This is useful when you are running
--- a code generator, switching git branches, or using other tools that
--- modify files in the background.
-vim.o.autoread = true -- Enable the basic autoread functionality.
+vim.o.autoread = true
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
   group = vim.api.nvim_create_augroup("auto_reload_on_change", { clear = true }),
   pattern = "*",
   callback = function()
-    -- This command checks the timestamp of the file on disk. If it's
-    -- more recent than the buffer's timestamp, Neovim will reload it.
-    -- We only run this in normal mode to avoid interruptions.
     if vim.fn.mode() == "n" then
       vim.cmd("checktime")
     end
@@ -40,8 +35,9 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGai
   desc = "Automatically reload files when they change on disk.",
 })
 
-
--- Bootstrap lazy.nvim
+-- ===================================================================
+--  Bootstrap lazy.nvim (Unchanged)
+-- ===================================================================
 local lazypath = vim.fn.stdpath("config") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -52,10 +48,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- ===================================================================
+--  Lazy Plugin List (UPDATED)
+-- ===================================================================
 require("lazy").setup({
   -- 1) Colorscheme:
   {
-         "folke/tokyonight.nvim",
+        "folke/tokyonight.nvim",
         priority = 1000,
         config = function()
             vim.cmd("colorscheme tokyonight-night")
@@ -82,11 +81,11 @@ require("lazy").setup({
     config = function()
       require("bufferline").setup({
         options = {
-          numbers = "ordinal", -- <== 🆕 This shows buffer numbers like 1, 2, 3
-          diagnostics             = "nvim_lsp",
+          numbers = "ordinal", 
+          diagnostics            = "nvim_lsp",
           show_buffer_close_icons = false,
-          show_close_icon         = false,
-          separator_style         = "slant",
+          show_close_icon       = false,
+          separator_style       = "slant",
           offsets = {
             {
               filetype   = "NvimTree",
@@ -110,40 +109,100 @@ require("lazy").setup({
     end,
   },
 
-  -- 8) LSP + completion
+  -- 8) LSP + completion (Unchanged)
   "neovim/nvim-lspconfig",
   "hrsh7th/nvim-cmp",
   "hrsh7th/cmp-nvim-lsp",
   "L3MON4D3/LuaSnip",
   "saadparwaiz1/cmp_luasnip",
 
-  -- 9) Treesitter
+  -- 9) Treesitter (Unchanged)
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-})
 
--- Keymaps
+  -- =================================================================
+  --  NEW PLUGINS FOR C# AND UNITY
+  -- =================================================================
+  
+  -- 10) MASON: Installs LSPs, DAPs, linters, etc.
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  
+  -- 11) MASON-LSPCONFIG: Bridges mason and lspconfig
+  { "williamboman/mason-lspconfig.nvim" },
+
+  -- 12) DAP: The main debugging plugin
+  { "mfussenegger/nvim-dap" },
+
+  -- 13) MASON-DAP: Bridges mason and the DAP
+  { "williamboman/mason-nvim-dap.nvim" },
+
+  -- 14) CSHARP.NVIM: Auto-configures C# LSP and Debugging
+  {
+    "iabdelkareem/csharp.nvim",
+    ft = "cs", -- Only load for C# files
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      -- Tell mason to install the .NET debugger
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "netcoredbg" },
+      })
+      
+      -- Setup csharp.nvim
+      require("csharp").setup({
+        lsp = true, -- Auto-configure the LSP
+        dap = true, -- Auto-configure the Debugger
+      })
+    end,
+  },
+  
+}) -- END OF lazy.setup
+
+-- ===================================================================
+--  Keymaps (Unchanged)
+-- ===================================================================
 vim.keymap.set("n", "<leader>e",  ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>",   { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>",    { noremap = true, silent = true })
 
--- Plugin configs that need to be loaded after setup:
--- Nvim Tree
+-- ===================================================================
+--  Plugin Configs (UPDATED)
+-- ===================================================================
+
+-- Nvim Tree (Unchanged)
 require("nvim-tree").setup()
 
--- Lualine
+-- Lualine (Unchanged)
 require("lualine").setup { options = { theme = "auto" } }
 
--- Treesitter configs
+-- Treesitter configs (UPDATED)
 require("nvim-treesitter.configs").setup {
   highlight = { enable = true },
   indent    = { enable = true },
-  ensure_installed = { "lua", "python", "cpp", "bash", "json" },
+  ensure_installed = { "lua", "python", "cpp", "bash", "json", "c_sharp" }, -- Added "c_sharp"
 }
 
--- LSP (example: Pyright)
-require("lspconfig").pyright.setup {}
+-- LSP (UPDATED)
+-- This will now automatically set up ALL servers you install with Mason
+-- (including pyright and csharp_ls)
+require("mason-lspconfig").setup({
+  ensure_installed = { "pyright", "csharp_ls" },
+  handlers = {
+    -- This is the default handler, it just starts the server
+    function(server_name)
+      require("lspconfig")[server_name].setup({})
+    end,
+  },
+})
 
--- Completion setup
+-- Completion setup (Unchanged)
 local cmp     = require("cmp")
 local luasnip = require("luasnip")
 
@@ -152,9 +211,9 @@ cmp.setup({
     expand = function(args) luasnip.lsp_expand(args.body) end,
   },
   mapping = cmp.mapping.preset.insert({
-    ["<Tab>"]    = cmp.mapping.select_next_item(),
-    ["<S-Tab>"]  = cmp.mapping.select_prev_item(),
-    ["<CR>"]     = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"]   = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<CR>"]    = cmp.mapping.confirm({ select = true }),
   }),
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
